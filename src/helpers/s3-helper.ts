0a735f78097145a1826f462320632554462eca8f
daiki510 <ddgg7755@gmail.com>
2023-03-10 16:37:46 +0900

    update

---
+++ src/helpers/s3-helper.ts
@@ -0,0 +1,81 @@
+// Ref:https://docs.aws.amazon.com/ja_jp/sdk-for-javascript/v3/developer-guide/javascript_s3_code_examples.html
+import { load } from 'ts-dotenv';
+import {
+  Bucket,
+  GetObjectCommand,
+  ListBucketsCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
+import { fromIni } from '@aws-sdk/credential-providers';
+
+const env = load({
+  BUCKET_NAME: String,
+  AWS_PROFILE: String,
+  S3_ENDPOINT: String,
+});
+
+class S3Helper {
+  private readonly s3Client: S3Client;
+
+  constructor() {
+    this.s3Client = new S3Client({
+      credentials: fromIni({ profile: env.AWS_PROFILE }),
+      endpoint: env.S3_ENDPOINT,
+    });
+  }
+
+  /**
+   * バケット一覧の取得
+   */
+  async listBuckets(): Promise<Bucket[] | undefined> {
+    try {
+      const data = await this.s3Client.send(new ListBucketsCommand({}));
+      console.log('Success', data.Buckets);
+      return data.Buckets;
+    } catch (err) {
+      console.log('Error', err);
+    }
+  }
+
+  /**
+   * オブジェクトの取得
+   */
+  async getObject(key: string): Promise<string | undefined> {
+    try {
+      const bucketParams = {
+        Bucket: env.BUCKET_NAME,
+        Key: key,
+      };
+      const data = await this.s3Client.send(new GetObjectCommand(bucketParams));
+      return (await data.Body?.transformToString()) ?? '';
+    } catch (err) {
+      console.log('Error', err);
+    }
+  }
+
+  /**
+   * オブジェクトのアップロード
+   */
+  async uploadObject(body: string, date: string): Promise<void> {
+    try {
+      const bucketParams = {
+        Bucket: env.BUCKET_NAME,
+        Key: `${date}_comic_list.json`,
+        Body: body,
+      };
+      const data = await this.s3Client.send(new PutObjectCommand(bucketParams));
+      console.log(
+        'Successfully uploaded object: ' +
+          bucketParams.Bucket +
+          '/' +
+          bucketParams.Key,
+        'Etag: ' + data.ETag
+      );
+    } catch (err) {
+      console.log('Error', err);
+    }
+  }
+}
+
+export const s3Helper = new S3Helper();
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,7 @@
 import { cdate } from 'cdate';
-import { load } from 'ts-dotenv';
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { fromIni } from '@aws-sdk/credential-providers';
-
-const env = load({
-  BUCKET_NAME: String,
-  AWS_PROFILE: String,
-  S3_ENDPOINT: String,
-});
-
-const s3Client = new S3Client({
-  credentials: fromIni({ profile: env.AWS_PROFILE }),
-  endpoint: env.S3_ENDPOINT,
-});
-
-const uploadObject = async (body: string, date: string) => {
-  try {
-    const inputParams = {
-      Bucket: env.BUCKET_NAME,
-      Key: `${date}_sample.json`,
-      Body: body,
-    };
-    const data = await s3Client.send(new PutObjectCommand(inputParams));
-    console.log(
-      'Successfully uploaded object: ' +
-        inputParams.Bucket +
-        '/' +
-        inputParams.Key,
-      'Etag: ' + data.ETag
-    );
-  } catch (err) {
-    console.log('Error', err);
-  }
-};
+import { s3Helper } from './helpers/s3-helper.js';
 
 (async () => {
-  console.log('==============start==============');
   //JSONデータの生成
   const currentDate = cdate().format('YYYYMMDDHHmmss');
   const data = {
@@ -45,6 +11,7 @@ const uploadObject = async (body: string, date: string) => {
   const json = JSON.stringify(data);
 
   //S3アップロード
-  await uploadObject(json, currentDate);
-  console.log('==============end==============');
+  await s3Helper.uploadObject(json, currentDate);
+
+  // await s3Helper.listBuckets();
 })();
